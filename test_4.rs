e9069038d427d423262e23ba3c2cf01318208f01
Abishek CHaulagain <abishekchaulagain5225@gmail.com>
2025-12-15 06:31:30 +0545

    updated

---
+++ test_4.rs
@@ -0,0 +1,14 @@
+fn divide(a: i32, b:i32) -> Result<i32, String>{
+    if b== 0{
+        Err("Cannot divide by zero".to_string())
+    }else{
+        Ok(a/b)
+    }
+}
+
+fn main(){
+    match divide(10, 0){
+        Ok(value) => println!("Result: {}", value),
+        Err(err) => println!("Error: {}", err),
+    }
+}
